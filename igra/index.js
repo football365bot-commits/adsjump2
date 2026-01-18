79fd64712bc3ba6b82a33ba1630b99430e6f8603
football365bot-commits <football365bot@gmail.com>
2026-01-19 00:26:25 +0200

    Update index.js

--- igra/index.js
+++ igra/index.js
@@ -1,1 +1,16 @@
+import { startGame } from './game.js';
+import { setupInput } from './player.js';
 
+const canvas = document.getElementById('game');
+const ctx = canvas.getContext('2d');
+
+function resize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+resize();
+window.addEventListener('resize', resize);
+
+setupInput(canvas);
+
+startGame(canvas, ctx);
