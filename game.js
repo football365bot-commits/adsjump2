a91a0f8e6db24d0dcfbda3e40ac5e9dde672eeb6
football365bot-commits <football365bot@gmail.com>
2026-01-22 22:31:54 +0200

    Update game.js

--- game.js
+++ game.js
@@ -556,10 +556,13 @@ function spawnEntities(isReset = false) {
 let inputX = 0;
 canvas.addEventListener('touchstart', e => {
     e.preventDefault();
+    if (gameState !== GameState.PLAYING) return; // <<< блокируем движение на паузе
     inputX = e.touches[0].clientX < canvas.width / 2 ? -1 : 1;
 }, { passive: false });
+
 canvas.addEventListener('touchend', e => {
     e.preventDefault();
+    if (gameState !== GameState.PLAYING) return; // <<< блокируем движение на паузе
     inputX = 0;
 }, { passive: false });
 
