c7f6a61684d05dd7bdf620b301d11e2039283231
football365bot-commits <football365bot@gmail.com>
2026-01-22 14:51:31 +0200

    Create pause.js

---
+++ pause.js
@@ -0,0 +1,125 @@
+// pauseUI.js
+
+export const GameState = {
+    PLAYING: 'playing',
+    PAUSED: 'paused',
+    MENU: 'menu'
+};
+
+export class PauseUI {
+    constructor(canvas, ctx, callbacks) {
+        this.canvas = canvas;
+        this.ctx = ctx;
+
+        this.callbacks = callbacks;
+
+        this.pauseButton = { x: 20, y: 20, w: 40, h: 40 };
+        this.pauseStart = 0;
+
+        this.buttons = {
+            resume: { x: 0, y: 0, w: 200, h: 40 },
+            menu: { x: 0, y: 0, w: 200, h: 40 }
+        };
+    }
+
+    draw(gameState) {
+        if (gameState === GameState.PLAYING) {
+            this.drawPauseButton();
+        }
+
+        if (gameState === GameState.PAUSED) {
+            this.drawOverlay();
+        }
+    }
+
+    drawPauseButton() {
+        const { ctx } = this;
+
+        ctx.strokeStyle = '#fff';
+        ctx.strokeRect(
+            this.pauseButton.x,
+            this.pauseButton.y,
+            this.pauseButton.w,
+            this.pauseButton.h
+        );
+
+        ctx.fillRect(this.pauseButton.x + 10, this.pauseButton.y + 8, 6, 24);
+        ctx.fillRect(this.pauseButton.x + 24, this.pauseButton.y + 8, 6, 24);
+    }
+
+    drawOverlay() {
+        const { ctx, canvas } = this;
+
+        ctx.fillStyle = 'rgba(0,0,0,0.6)';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        const box = {
+            w: 300,
+            h: 220,
+            x: canvas.width / 2 - 150,
+            y: canvas.height / 2 - 110
+        };
+
+        ctx.fillStyle = '#222';
+        ctx.fillRect(box.x, box.y, box.w, box.h);
+
+        ctx.strokeStyle = '#fff';
+        ctx.strokeRect(box.x, box.y, box.w, box.h);
+
+        ctx.fillStyle = '#fff';
+        ctx.font = '28px Arial';
+        ctx.textAlign = 'center';
+        ctx.fillText('ПАУЗА', canvas.width / 2, box.y + 40);
+
+        this.drawButtons(box);
+    }
+
+    drawButtons(box) {
+        this.buttons.resume.x = box.x + 50;
+        this.buttons.resume.y = box.y + 80;
+
+        this.buttons.menu.x = box.x + 50;
+        this.buttons.menu.y = box.y + 140;
+
+        this.drawButton(this.buttons.resume, 'Продолжить');
+        this.drawButton(this.buttons.menu, 'Главное меню');
+    }
+
+    drawButton(btn, text) {
+        const { ctx } = this;
+
+        ctx.strokeStyle = '#fff';
+        ctx.strokeRect(btn.x, btn.y, btn.w, btn.h);
+
+        ctx.fillStyle = '#fff';
+        ctx.font = '20px Arial';
+        ctx.textAlign = 'center';
+        ctx.fillText(text, btn.x + btn.w / 2, btn.y + 26);
+    }
+
+    handleClick(x, y, gameState) {
+        if (gameState === GameState.PLAYING && this.isInside(x, y, this.pauseButton)) {
+            this.pauseStart = Date.now();
+            this.callbacks.onPause();
+            return true;
+        }
+
+        if (gameState === GameState.PAUSED) {
+            if (this.isInside(x, y, this.buttons.resume)) {
+                this.callbacks.onResume(Date.now() - this.pauseStart);
+                return true;
+            }
+
+            if (this.isInside(x, y, this.buttons.menu)) {
+                this.callbacks.onMenu();
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    isInside(x, y, b) {
+        return x > b.x && x < b.x + b.w && y > b.y && y < b.y + b.h;
+    }
+}
