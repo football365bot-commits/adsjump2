e089c369abf0286dd35a4f6eb698c2b445fce235
football365bot-commits <football365bot@gmail.com>
2026-01-24 02:32:39 +0200

    Update pause.js

--- pause.js
+++ pause.js
@@ -72,7 +72,7 @@ export class PauseUI {
         // Рисуем монетки только на Game Over
         if (gameState === GameState.GAME_OVER) {
             ctx.font = '18px Arial';
-            ctx.fillText(`Монетки: ${coins.toFixed(2)}`, canvas.width / 2, box.y + 60);
+            ctx.fillText(`Монетки: ${coins.toFixed(2)}`, canvas.width / 2, box.y + 40);
         }
 
         this.drawButtons(box, gameState);
