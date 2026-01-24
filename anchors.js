4e6d5f963079ee10477efb0894617783f8413e64
football365bot-commits <football365bot@gmail.com>
2026-01-24 19:10:17 +0200

    Create anchors.js

---
+++ anchors.js
@@ -0,0 +1,7 @@
+// anchors.js
+export const PlayerAnchors = {
+    hand: { x: 0.75, y: 0.5 },   // координаты в процентах от размера игрока
+    head: { x: 0.5, y: 0 },
+    chest: { x: 0.5, y: 0.3 },
+    feet: { x: 0.5, y: 1 },
+};
