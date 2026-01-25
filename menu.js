c51633c18caa28636db6896df2fb138cab3cf97e
football365bot-commits <football365bot@gmail.com>
2026-01-25 20:20:33 +0200

    Update menu.js

--- menu.js
+++ menu.js
@@ -13,9 +13,9 @@ export class Menu {
         ];
 
         this.buttonWidth = 120;
-        this.buttonHeight = 50;
+        this.buttonHeight = 100;
         this.buttonGap = 25;
-        this.startX = 15; // отступ слева
+        this.startX = 10; // отступ слева
         this.startY = null;
     }
 
