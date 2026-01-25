d95cf8f9da0cc4716372faa3c77c2e36635209e9
football365bot-commits <football365bot@gmail.com>
2026-01-25 15:59:09 +0200

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,7 +1,8 @@
 // menu.js
-export class Menu {
-    constructor(onStartGame) {
-        // создаём canvas для меню
+import { startGame } from './game.js'; // функция, которая запускает твой Game.js
+
+class Menu {
+    constructor() {
         this.canvas = document.createElement('canvas');
         this.ctx = this.canvas.getContext('2d');
         this.canvas.width = window.innerWidth;
@@ -9,12 +10,9 @@ export class Menu {
         this.canvas.style.position = 'absolute';
         this.canvas.style.top = '0';
         this.canvas.style.left = '0';
-        this.canvas.style.zIndex = 10; // поверх игрового canvas
+        this.canvas.style.zIndex = 10;
         document.body.appendChild(this.canvas);
 
-        this.onStartGame = onStartGame;
-
-        // кнопка "Играть"
         this.buttons = {
             play: {
                 x: this.canvas.width / 2 - 100,
@@ -25,25 +23,22 @@ export class Menu {
             }
         };
 
-        // обработчик клика по canvas
         this.canvas.addEventListener('click', (e) => this.handleClick(e));
-        this.draw(); // сразу отрисовываем меню
+
+        this.draw();
     }
 
     draw() {
         const { ctx, canvas } = this;
 
-        // фон
         ctx.fillStyle = '#111';
         ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-        // заголовок
         ctx.fillStyle = '#fff';
         ctx.font = '48px Arial';
         ctx.textAlign = 'center';
         ctx.fillText('Главное Меню', canvas.width / 2, canvas.height / 3);
 
-        // кнопка
         for (const key in this.buttons) {
             const b = this.buttons[key];
             ctx.strokeStyle = '#fff';
@@ -60,14 +55,10 @@ export class Menu {
         const x = e.clientX - rect.left;
         const y = e.clientY - rect.top;
 
-        for (const key in this.buttons) {
-            const b = this.buttons[key];
-            if (x > b.x && x < b.x + b.w && y > b.y && y < b.y + b.h) {
-                if (key === 'play') {
-                    this.hide();
-                    this.onStartGame();
-                }
-            }
+        const b = this.buttons.play;
+        if (x > b.x && x < b.x + b.w && y > b.y && y < b.y + b.h) {
+            this.hide();
+            startGame(); // запускаем Game.js
         }
     }
 
@@ -80,3 +71,6 @@ export class Menu {
         this.draw();
     }
 }
+
+// создаём меню при загрузке
+new Menu();
